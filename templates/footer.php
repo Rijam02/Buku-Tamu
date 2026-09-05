445b4679d26c854237458e848e12e3d529956627
RIZAM <rijarmali@gmail.com>
2026-09-05 16:58:37 +0700

    templating app buku tamu dengan sb admin 2

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>Copyright &copy; ZieBukuTamu</span>
                     </div>
                 </div>
             </footer>
