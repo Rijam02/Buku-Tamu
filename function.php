27a96ca6df97ee040df188e0b101d97b2f42799f
RIZAM <rijarmali@gmail.com>
2026-09-20 13:13:00 +0700

    Update

---
+++ function.php
@@ -0,0 +1,56 @@
+<?php
+require_once('koneksi.php');
+
+function query($query) {
+    global $koneksi;
+    $result = mysqli_query($koneksi, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function tambah_tamu($data) {
+
+    global $koneksi;
+
+    $kode        = htmlspecialchars($data["id_tamu"]);
+    $tanggal     = date("Y-m-d");
+    $nama_tamu   = htmlspecialchars($data["nama_tamu"]);
+    $alamat      = htmlspecialchars($data["alamat"]);
+    $no_hp       = htmlspecialchars($data["no_hp"]);
+    $bertemu     = htmlspecialchars($data["bertemu"]);
+    $kepentingan = htmlspecialchars($data["kepentingan"]);
+
+    $query = "INSERT INTO buku_tamu VALUES ('$kode', '$tanggal', '$nama_tamu', '$alamat', '$no_hp', '$bertemu', '$kepentingan')";
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+
+    // 
+}
+
+function ubah_tamu($data) {
+    
+    global $koneksi;
+
+    $id        = htmlspecialchars($data["id_tamu"]);
+    $nama_tamu   = htmlspecialchars($data["nama_tamu"]);
+    $alamat      = htmlspecialchars($data["alamat"]);
+    $no_hp       = htmlspecialchars($data["no_hp"]);
+    $bertemu     = htmlspecialchars($data["bertemu"]);
+    $kepentingan = htmlspecialchars($data["kepentingan"]);
+
+    $query = "UPDATE buku_tamu SET
+              nama_tamu     = '$nama_tamu',
+              alamat        = '$alamat',
+              no_hp         = '$no_hp',
+              bertemu       = '$bertemu',
+              kepentingan   = '$kepentingan'
+              WHERE id_tamu = '$id'";
+    
+    mysqli_query($koneksi, $query);
+
+    return mysqli_affected_rows($koneksi);
+}
